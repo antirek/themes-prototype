61eee003713c423a66ef157906f3e628daded6fa
sergey <110@mobilon.ru>
2025-08-30 22:35:40 +0700

    fix

--- src/components/CardFooter/types.ts
+++ src/components/CardFooter/types.ts
@@ -4,8 +4,18 @@
 export interface CardFooterThemeCSSVariables {
   /** Фон футера карточки */
   '--thepro-cardfooter-bg': string;
-  /** Цвет текста футера */
+  /** Цвет текста футера карточки */
   '--thepro-cardfooter-text': string;
-  /** Цвет границы футера */
+  /** Цвет границы футера карточки */
   '--thepro-cardfooter-border': string;
+  /** Отступы футера карточки */
+  '--thepro-cardfooter-padding': string;
+  /** Отступы футера карточки на мобильных устройствах */
+  '--thepro-cardfooter-padding-mobile': string;
+  /** Отступы футера карточки на маленьких экранах */
+  '--thepro-cardfooter-padding-small': string;
+  /** Размер шрифта футера карточки */
+  '--thepro-cardfooter-font-size': string;
+  /** Размер шрифта футера карточки на маленьких экранах */
+  '--thepro-cardfooter-font-size-small': string;
 }
