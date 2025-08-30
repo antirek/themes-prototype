27465e427f7ab797622b56eabbd7ad0532f89fa0
sergey <110@mobilon.ru>
2025-08-30 22:28:04 +0700

    fix

--- src/components/CardBody/types.ts
+++ src/components/CardBody/types.ts
@@ -4,8 +4,12 @@
 export interface CardBodyThemeCSSVariables {
   /** Фон тела карточки */
   '--thepro-cardbody-bg': string;
-  /** Цвет текста тела */
+  /** Цвет текста тела карточки */
   '--thepro-cardbody-text': string;
   /** Фон оверлея (псевдоэлемент ::after) */
   '--thepro-cardbody-overlay': string;
+  /** Семейство шрифтов тела карточки */
+  '--thepro-cardbody-font-family': string;
+  /** Выравнивание текста тела карточки */
+  '--thepro-cardbody-text-align': string;
 }
